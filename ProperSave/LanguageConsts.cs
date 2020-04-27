8036c1e61b61442b3bb180612366d669b3be621c
Юрасов Денис <d.yurasov@powwwer.io>
2020-04-27 16:52:02 +0700

    Fixed typo, deleted old language tokens

--- ProperSave/LanguageConsts.cs
+++ ProperSave/LanguageConsts.cs
@@ -4,7 +4,5 @@
     {
         public const string PS_TITLE_CONTINUE_DESC = "PS_TITLE_CONTINUE_DESC";
         public const string PS_TITLE_CONTINUE = "PS_TITLE_CONTINUE";
-        public const string PS_TITLE_SAVE = "PS_TITLE_SAVE";
-        public const string PS_CHAT_SAVE = "PS_CHAT_SAVE";
     }
 }
